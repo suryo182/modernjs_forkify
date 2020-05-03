2d414f5deb7a2b04e52804c9856c45a177ad8fc8
Suryo Adi Nugroho <40680835+suryo182@users.noreply.github.com>
2020-05-03 17:05:31 +0700

    build search view part 2

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -9,6 +9,24 @@ export const clearResults = () => {
   elements.searchResList.innerHTML = '';
 }
 
+// 'Pasta with tomato and spinach'
+/*
+0
+*/
+const limitRecipeTitle = (title, limit = 17) => {
+  const newTitle = [];
+  if (title.length > limit) {
+    title.split(' ').reduce((acc, cur) => {
+      if (acc + cur.length <= limit) {
+        newTitle.push(cur);
+      }
+      return acc + cur.length;
+    }, 0);
+    return `${newTitle.join(' ')} ...`;
+  }
+  return title;
+}
+
 const renderRecipe = recipe => {
   const markup = /*html*/`
   <li>
@@ -17,7 +35,7 @@ const renderRecipe = recipe => {
           <img src="${recipe.image_url}" alt="${recipe.title}">
       </figure>
       <div class="likes__data">
-          <h4 class="likes__name">${recipe.title}</h4>
+          <h4 class="likes__name">${limitRecipeTitle(recipe.title)}</h4>
           <p class="likes__author">${recipe.publisher}</p>
       </div>
     </a>
