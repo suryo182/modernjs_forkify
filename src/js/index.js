87b08a3d3517586fcfaf0afa823080ba664ae9cc
Suryo Adi Nugroho <40680835+suryo182@users.noreply.github.com>
2020-05-06 00:25:03 +0700

    Build recipe model part 1

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import Search from './models/Search';
+import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
 import { elements, renderLoader, clearLoader } from './views/base';
 /** Global state of the app
@@ -9,6 +10,9 @@ import { elements, renderLoader, clearLoader } from './views/base';
  */
 const state = {};
 
+/** 
+ * SEARCH CONTROLLER
+ */
 const controlSearch = async () => {
   // 1) Get the query from the view
   const query = searchView.getInput();
@@ -43,3 +47,10 @@ elements.searchResPages.addEventListener('click', e => {
     searchView.renderResults(state.search.result, goToPage);
   }
 });
+
+/** 
+ * SEARCH CONTROLLER
+ */
+const r = new Recipe(47746);
+r.getRecipe();
+console.log(r);
