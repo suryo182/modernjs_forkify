b6c3d530c9fabdd56d24e99ec102cc55bcc389a5
Suryo Adi Nugroho <40680835+suryo182@users.noreply.github.com>
2020-05-06 14:37:44 +0700

    Building recipe controller

--- src/js/index.js
+++ src/js/index.js
@@ -25,12 +25,18 @@ const controlSearch = async () => {
     searchView.clearInput();
     searchView.clearResults();
     renderLoader(elements.searchRes);
-    // 4) Search for recipes
-    await state.search.getResults();
 
-    // 5) Render results on UI
-    clearLoader();
-    searchView.renderResults(state.search.result);
+    try {
+      // 4) Search for recipes
+      await state.search.getResults();
+  
+      // 5) Render results on UI
+      clearLoader();
+      searchView.renderResults(state.search.result);
+    } catch (err) {
+      alert('Something wrong with the search...');
+      clearLoader();
+    }
   }
 }
 
@@ -51,6 +57,33 @@ elements.searchResPages.addEventListener('click', e => {
 /** 
  * SEARCH CONTROLLER
  */
-const r = new Recipe(47746);
-r.getRecipe();
-console.log(r);
+const controlRecipe = async () => {
+  // Get ID from url
+  const id = window.location.hash.replace('#', '');
+  console.log(id);
+
+  if (id) {
+    // Prepare UI for changes
+
+    // Create new recipe object
+    state.recipe = new Recipe(id);
+
+    try {
+      // Get recipe data
+      await state.recipe.getRecipe();
+      // Calculate servings and time
+      state.recipe.calcTime();
+      state.recipe.calcServings();
+      // Render recipe
+      console.log(state.recipe);
+    } catch (err) {
+      alert('Error processing recipe!');
+    }
+  }
+}
+
+
+window.addEventListener('hashchange', controlRecipe);
+window.addEventListener('load', controlRecipe);
+
+['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
